5b9951d1d6d4a297d9a06e38c6ab2c44f2c7e6d8
Marius786 <marius@presslabs.com>
2014-06-19 18:05:58 +0300

    fix small error

--- tests/test-git-wrapper.php
+++ tests/test-git-wrapper.php
@@ -1,6 +1,6 @@
 <?php class Test_Git_Wrapper extends WP_UnitTestCase {
 	function test_class_exists_git_wrapper() {
-		$this->assertTrue( class_exists( 'Git_Wrapperi' ) );
+		$this->assertTrue( class_exists( 'Git_Wrapper' ) );
 	}
 }
 
