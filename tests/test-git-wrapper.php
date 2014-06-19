a45833cbb900cf43fd4d7c904ef8a617d03f09ca
Marius786 <marius@presslabs.com>
2014-06-19 18:00:46 +0300

    Add WordPress code standard format

--- tests/test-git-wrapper.php
+++ tests/test-git-wrapper.php
@@ -1,9 +1,6 @@
-<?php
-class Test_Git_Wrapper extends WP_UnitTestCase
-{
-  function test_class_exists_git_wrapper() {
-    $this->assertTrue( class_exists('Git_Wrapper') );
-  }
-
+<?php class Test_Git_Wrapper extends WP_UnitTestCase {
+	function test_class_exists_git_wrapper() {
+		$this->assertTrue( class_exists( 'Git_Wrapperi' ) );
+	}
 }
 
