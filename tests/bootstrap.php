b2aa10a2e8e580123940073fdbbd21f11319cd22
Marius786 <marius@presslabs.com>
2014-06-24 18:22:55 +0300

    load & activate the `gitium` plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,12 @@
 <?php
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-require_once $_tests_dir . '/includes/functions.php';
+define( 'WP_PLUGIN_DIR', dirname( __DIR__ ) );
 
-function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../gitium/gitium.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( 'gitium/gitium.php' ),
+);
 
 require $_tests_dir . '/includes/bootstrap.php';
-
