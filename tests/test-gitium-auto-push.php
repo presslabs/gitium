2cb494e9b377dc590b01ae46aa9c6e588625a6b5
Marius786 <marius@presslabs.com>
2014-07-21 17:04:22 +0300

    add new test for `_gitium_status`

--- tests/test-gitium-auto-push.php
+++ tests/test-gitium-auto-push.php
@@ -8,4 +8,20 @@ class Test_Gitium_Auto_Push extends Gitium_UnitTestCase {
 		gitium_auto_push();
 		$this->assertEmpty( $git->get_uncommited_changes() );
 	}
+
+	function test_gitium_status() {
+		global $git;
+
+		// 1.add local change
+		$this->_add_uncommited_changes_locally( 'locla', TRUE );
+
+		// 2.add remote changes and commit them(add two behind commits)
+		$this->_add_uncommited_changes_remotely( 'one', TRUE );
+		$this->_add_uncommited_changes_remotely( 'two', TRUE );
+		$git->fetch_ref();
+
+		// 3.test if the changes are visible in status call
+		$status = _gitium_status();
+		$this->assertStringEndsWith( '[ahead 1, behind 2]', $status[0] );
+	}
 }
