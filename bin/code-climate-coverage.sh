048ce5b794d1ef57c76b3303bd19f4ebc4b544d9
Calin Don <calin.don@gmail.com>
2014-07-30 15:58:26 +0300

    Fix code climent test reporter

--- bin/code-climate-coverage.sh
+++ bin/code-climate-coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 CODECLIMATE_REPO_TOKEN=819c356e2d022f98e4e2717dbd459c3801fef95241a2db7261596a0482dd6ca3 vendor/bin/test-reporter --stdout > codeclimate.json
-curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v1.0.1-dev)' https://codeclimate.com/test_reports
+curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
