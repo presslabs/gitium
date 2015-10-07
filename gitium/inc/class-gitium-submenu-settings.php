6d967e50a6ba86a632325e3e3fcbf185336d7068
Marius Veltan <marius@hipmill.com>
2015-10-07 14:34:33 +0300

    Add a "Ping Now!" button for calling the update webhook.

--- gitium/inc/class-gitium-submenu-settings.php
+++ gitium/inc/class-gitium-submenu-settings.php
@@ -62,7 +62,8 @@ class Gitium_Submenu_Settings extends Gitium_Menu {
 			<td>
 			  <p><code id="webhook-url"><?php echo esc_url( gitium_get_webhook() ); ?></code>
 			  <?php if ( ! defined( 'GIT_WEBHOOK_URL' ) || GIT_WEBHOOK_URL == '' ) : ?>
-			  <input type="submit" name="GitiumSubmitRegenerateWebhook" class="button" value="<?php _e( 'Regenerate Webhook', 'gitium' ); ?>" /></p>
+			  <input type="submit" name="GitiumSubmitRegenerateWebhook" class="button" value="<?php _e( 'Regenerate Webhook', 'gitium' ); ?>" />
+                          <a class="button" href="<?php echo esc_url( gitium_get_webhook() ); ?>" target="_blank">Ping now!</a></p>
 			  <?php endif; ?>
 			  <p class="description"><?php _e( 'Pinging this URL triggers an update from remote repository.', 'gitium' ); ?></p>
 			</td>
