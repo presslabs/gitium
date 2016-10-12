c795055be038522c1dd799d403e4bb7df9b52844
Marius786 <marius@presslabs.com>
2016-10-12 16:07:56 +0300

    Put the logs off by default

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,7 @@ $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
 define( 'WP_PLUGIN_DIR', dirname( __DIR__ ) );
+define( 'WP_DEBUG', False );
 
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'gitium/gitium.php' ),
