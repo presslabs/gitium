dc886214a0de2106a59fce71c0a9896856bff114
Uibar Ion-Cristian <cristi@presslabs.com>
2017-05-23 13:01:51 +0300

    Moved bin folder inside presslabs env folder

--- presslabs/bin/install-wp-tests.sh
+++ presslabs/bin/install-wp-tests.sh
@@ -14,8 +14,8 @@ DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
 SKIP_DB_CREATE=${6-false}
 
-WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
+WP_TESTS_DIR=${WP_TESTS_DIR-/application/wp-tests}
+WP_CORE_DIR=${WP_CORE_DIR-/application/}
 
 echo -ne '[###                           ](10%)\r'
 
