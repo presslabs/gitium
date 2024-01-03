33e92708d32cb4f897e53e35be0607f93f55450e
petero-dk <2478689+petero-dk@users.noreply.github.com>
2024-01-03 11:52:05 +0100

    Update class-git-wrapper.php

--- gitium/inc/class-git-wrapper.php
+++ gitium/inc/class-git-wrapper.php
@@ -15,7 +15,8 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define('GITIGNORE', <<<EOF
+if (!defined('GITIGNORE'))
+    define('GITIGNORE', <<<EOF
 *.log
 *.swp
 *.back
