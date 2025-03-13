216c284341e11ba4864830de40ded4a357176cbd
DenisFlorin <38504865+DenisFlorin@users.noreply.github.com>
2025-03-13 14:14:57 +0200

    Update gitium.php

--- gitium/gitium.php
+++ gitium/gitium.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.presslabs.com
  * Text Domain:     gitium
  * Domain Path:     /languages
- * Version:         1.0.7
+ * Version:         1.2.0
  * License:         GPL3
  *
  * Gitium provides automatic git version control and deployment for
